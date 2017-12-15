44106237c5ed4a601c1799fa64417dc359fd8a31
Gerardo Ortega <geraldavid7@gmail.com>
2017-12-15 14:29:03 -0600

    Starting CardPointe support

--- lib/card_connect.rb
+++ lib/card_connect.rb
@@ -4,6 +4,13 @@ require 'card_connect/utils'
 require 'card_connect/error'
 require 'card_connect/configuration'
 require 'card_connect/connection'
+require 'card_connect/endpoint'
+
+require 'card_connect/card_pointe/card_pointe_endpoint'
+require 'card_connect/card_pointe/authorization/authorization'
+require 'card_connect/card_pointe/authorization/authorization_request'
+require 'card_connect/card_pointe/authorization/authorization_response'
+
 
 module CardConnect
   class << self
