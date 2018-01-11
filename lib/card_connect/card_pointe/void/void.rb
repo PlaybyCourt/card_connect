10363dec410b43b70f13bd8ba43bde520801a0bf
Gerardo Ortega <geraldavid7@gmail.com>
2018-01-11 11:55:48 -0600

    Including void service

---
+++ lib/card_connect/card_pointe/void/void.rb
@@ -0,0 +1,15 @@
+module CardConnect
+  module CardPointe
+    class Void < ServiceEndpoint
+      # Initializes a Void Service
+      #
+      # @param connection [CardConnect::Connection]
+      # @return CardConnect::Service::Void
+      def initialize(connection = CardConnect.connection)
+        super(connection)
+        @resource_name = '/void'
+        @rest_method = 'put'
+      end
+    end
+  end
+end
---
+++ lib/card_connect/card_pointe/void/void_response.rb
@@ -0,0 +1,41 @@
+module CardConnect
+  module CardPointe
+    class VoidResponse
+      include Utils
+
+      FIELDS = [:merchid, :amount, :currency, :retref, :authcode,
+                :respcode, :respproc, :respstat, :resptext].freeze
+
+      attr_accessor(*FIELDS)
+      attr_reader :errors
+
+      STATUS_APPROVED = 'A'.freeze
+      STATUS_RETRY = 'B'.freeze
+      STATUS_DECLINED = 'C'.freeze
+
+      def initialize(response)
+        set_attributes(response, FIELDS)
+        @errors = []
+        process_errors
+      end
+
+      def success?
+        errors.empty?
+      end
+
+      def body
+        body = {}
+        FIELDS.each do |attr|
+          body.merge!(attr => send(attr))
+        end
+        body
+      end
+
+      private
+
+      def process_errors
+        @errors << resptext if [STATUS_RETRY, STATUS_DECLINED].include?(respstat)
+      end
+    end
+  end
+end
